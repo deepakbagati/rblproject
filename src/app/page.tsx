9ae777403563f3fcd25aefa069656a89bdc93d16
deepakbagati <deepakbagati2708@gmail.com>
2024-04-26 20:13:47 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { BackgroundGradientAnimation } from "./componenets/ui/background-gradient-animation";
 import { BackgroundGradientAnimationDemo } from "./componenets/BgAnimation";
 import Footer from "./componenets/Footer"
+import  {HeroParallaxDemo}  from "./componenets/Hero";
 
 export default function Home() {
   return (
    <>
 
    <BackgroundGradientAnimationDemo/>
+  <HeroParallaxDemo/>
    <Footer/>
     
    </>
